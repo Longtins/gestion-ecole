4d9f08a87ad25814cfece278e0f55220545b6719
sarah <sarah.longtin1998@gmail.com>
2026-04-21 15:59:28 -0400

    attribut telephoe dans eleve

--- eleve.py
+++ eleve.py
@@ -1,7 +1,9 @@
 class Eleve:
-    def __init__(self, nom, prenom):
+    def __init__(self, nom, prenom, num_etudiant, telephone):
         self.nom = nom
         self.prenom = prenom
+        self.num_etudiant = num_etudiant
+        self.telephone = telephone
 
-    def __str__(self):
-        return f"{self.prenom} {self.nom}"
+def __str__(self):
+    return f"{self.prenom} {self.nom} (numéro: {self.num_etudiant}) — {self.telephone}"
