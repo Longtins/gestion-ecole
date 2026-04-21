76572a56fa7df9557efb503332a8716e18a5373e
sarah <sarah.longtin1998@gmail.com>
2026-04-21 15:58:04 -0400

    attributs courriel dans eleve

--- eleve.py
+++ eleve.py
@@ -1,7 +1,9 @@
 class Eleve:
-    def __init__(self, nom, prenom):
+    def __init__(self, nom, prenom,num_etudiant,courriel):
         self.nom = nom
         self.prenom = prenom
+        self.num_etudiant = num_etudiant
+        self.courriel = courriel
 
     def __str__(self):
-        return f"{self.prenom} {self.nom}"
+        return f"{self.prenom} {self.nom} (numéro: {self.num_etudiant}) — {self.courriel}"
