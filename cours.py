7c2c153c163c7d80d46fb5f9c3824bf5e03db2b1
sarah <sarah.longtin1998@gmail.com>
2026-04-21 16:23:42 -0400

    ajout attribut description et sigle_cours

--- cours.py
+++ cours.py
@@ -1,8 +1,9 @@
 class Cours:
-    def __init__(self, nom, enseignant):
+    def __init__(self, nom,sigle_cours, enseignant,description = None):
         self.nom = nom
+        self.sigle_cours = sigle_cours
         self.enseignant = enseignant
-        self.eleves = []
+        self.description = description
 
     def inscrire(self, eleve):
         self.eleves.append(eleve)
